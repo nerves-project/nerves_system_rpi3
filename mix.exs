a0231e98a4bd889673b555a0f6f34d8140519136
Justin Schneck <jschneck@mac.com>
2016-04-04 16:35:27 -0400

    point deps to github

--- mix.exs
+++ mix.exs
@@ -16,9 +16,9 @@ defmodule NervesSystemRpi3.Mixfile do
   end
 
   defp deps do
-    [{:nerves_system, path: "../nerves_system"},
-     {:nerves_system_br, path: "../nerves-system-br"},
-     {:nerves_toolchain_arm_unknown_linux_gnueabihf, path: "../nerves_toolchain_arm_unknown_linux_gnueabihf"}]
+    [{:nerves_system, github: "nerves-project/nerves_system"},
+     {:nerves_system_br, github: "nerves-project/nerves-system-br"},
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, github: "nerves-project/nerves_toolchain_arm_unknown_linux_gnueabihf"}]
   end
 
   defp description do
