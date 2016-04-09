39c0cbe9eac221305b1920904429f2e79604d767
Justin Schneck <jschneck@mac.com>
2016-04-09 19:57:41 -0400

    Updated nerves config and removed Config.in

--- nerves.exs
+++ nerves.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :nerves_system_rpi3, :nerves_env,
   type: :system,
-  build_platform: Nerves.System.Platforms.BR,
   bakeware: [target: "rpi3", recipe: "nerves/rpi3"],
-  ext: [
+  build_platform: Nerves.System.Platforms.BR,
+  build_config: [
     defconfig: "nerves_defconfig"
   ]
