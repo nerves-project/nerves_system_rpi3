0dc93ffa768162f903e686227b824891e925317f
Justin Schneck <jschneck@mac.com>
2017-08-08 12:14:18 -0400

    nerves_system_br 0.13.5

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule NervesSystemRpi3.Mixfile do
   defp deps do
     [
       {:nerves, "~> 0.7", runtime: false },
-      {:nerves_system_br, "~> 0.13.3", runtime: false },
+      {:nerves_system_br, "~> 0.13.5", runtime: false },
       {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.11.0", runtime: false}
     ]
   end
