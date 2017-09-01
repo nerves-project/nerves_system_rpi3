a86fe04440ce8f91e321a9d982501c588287df6a
Justin Schneck <jschneck@mac.com>
2017-09-01 10:58:19 -0400

    Update circle ci config and test framework dependencies

--- test/mix.exs
+++ test/mix.exs
@@ -37,7 +37,7 @@ defmodule Test.Mixfile do
     [
       {:nerves, "~> 0.7", runtime: false},
       {:nerves_system_rpi3, path: "../", runtime: false},
-      {:nerves_system_test, github: "mobileoverlord/nerves_system_test"}
+      {:nerves_system_test, github: "nerves-project/nerves_system_test"}
       #{:nerves_system_test, path: "../../nerves_system_test"}
     ]
   end
