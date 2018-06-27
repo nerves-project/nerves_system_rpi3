045ae013c9684c1e16f1ec88e3e344f04b2cd099
Justin Schneck <jschneck@mac.com>
2018-06-27 09:26:10 -0400

    Bump nerves_system_br to 1.3.0

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule NervesSystemRpi3.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.0", runtime: false},
-      {:nerves_system_br, "1.2.2", runtime: false},
+      {:nerves_system_br, "1.3.0", runtime: false},
       {:nerves_toolchain_arm_unknown_linux_gnueabihf, "1.0.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: :dev}
