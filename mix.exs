24466fd0615f3b158455ee232c500c079a738d04
Frank Hunleth <fhunleth@troodon-software.com>
2022-02-06 16:19:30 -0500

    Pass compilation options via TARGET_GCC_FLAGS
    This adds Raspberry Pi 3-specific build options for compiling ports and
    NIFs. Then makes it possible for the NIF/port build options to be the
    same as those used for other C/C++ code in the system (including the
    Erlang runtime).
    
    This also forces `:nerves` `v1.7.15` if using `v1.7.x`. Since
    `TARGET_GCC_FLAGS` was released in a patch release (it's technically
    experimental for now), it's necessary to use `v1.7.15` for this to work.

--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule NervesSystemRpi3.MixProject do
         {"TARGET_ARCH", "arm"},
         {"TARGET_CPU", "cortex_a53"},
         {"TARGET_OS", "linux"},
-        {"TARGET_ABI", "gnueabihf"}
+        {"TARGET_ABI", "gnueabihf"},
+        {"TARGET_GCC_FLAGS",
+         "-mabi=aapcs-linux -mfpu=neon-vfpv4 -marm -fstack-protector-strong -mfloat-abi=hard -mcpu=cortex-a53 -fPIE -pie -Wl,-z,now -Wl,-z,relro"}
       ],
       checksum: package_files()
     ]
@@ -64,7 +66,7 @@ defmodule NervesSystemRpi3.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.4", runtime: false},
+      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.15", runtime: false},
       {:nerves_system_br, "1.18.4", runtime: false},
       {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.5.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
