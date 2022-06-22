e00ce849d9dba9f5e662815533b697bc30a69d93
Masatoshi N <nishiguchi.masa@gmail.com>
2022-06-22 19:40:28 -0400

    Relax nerves version for 1.8.0

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule NervesSystemRpi3.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.15", runtime: false},
+      {:nerves, "~> 1.6.0 or ~> 1.7.15 or ~> 1.8.0", runtime: false},
       {:nerves_system_br, "1.20.0", runtime: false},
       {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.5.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
